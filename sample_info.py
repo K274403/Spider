a6f85d1b4c1f3233586b11af63a12b4ed2d553ad
K274403 <158284550+K274403@users.noreply.github.com>
2025-01-05 06:40:41 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'LuciferMoringstar_Robot'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
+API_ID = 
+API_HASH = ''
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+ADMINS = [1798348973, 'admin123', ]
+CHANNELS = ['-1002148604082', 'Mo_Tech_YT']
 AUTH_USERS = []
-AUTH_CHANNEL = None
+AUTH_CHANNEL = '-1002023191399'
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = ""
+DATABASE_NAME = ''
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
@@ -28,5 +28,5 @@ START_MSG = """
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
 
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
+SHARE_BUTTON_TEXT = 'Checkout {Rkmoviezip} for searching files'
+INVITE_MSG = 'Please join @rkmovieszip to use this bot'
